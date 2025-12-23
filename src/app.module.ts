4e1d1ccfc0e0effda2da3d32e892e0110b399476
thanhloi <marcomoi395@gmail.com>
2025-12-23 22:54:07 +0700

    feat: add Joi validation for environment variables in configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,21 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import Joi from 'joi';
 
 @Module({
     imports: [
         PrismaModule,
         ConfigModule.forRoot({
             isGlobal: true,
+            validationSchema: Joi.object({
+                DATABASE_URL: Joi.string().required(),
+                JWT_SECRET: Joi.string().required(),
+            }),
+            validationOptions: {
+                allowUnknow: true,
+                aboutEarly: false,
+            },
         }),
         AuthModule,
         UserModule,
